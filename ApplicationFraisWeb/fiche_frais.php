79d6db09ff56067032e8c13252617f0259430d00
Daniel Paul <paul.daniel.69@hotmail.fr>
2019-04-21 01:39:08 +0200

    fiche frais

--- ApplicationFraisWeb/fiche_frais.php
+++ ApplicationFraisWeb/fiche_frais.php
@@ -1,4 +1,5 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 $host_name = 'localhost';
 $database = 'gsb';
@@ -6,18 +7,17 @@ $user_name = 'root';
 $password = 'root';
 
 $bdd = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
-// Connexion a la base de données => new PDO( ... )
-$query = 'SELECT `id`, `mois`, `annee`, `etat_id`, `utilisateur_id` FROM fiche_frais';
-$d = $bdd->query($query);
-$fiches = $d->fetchAll();
+
+$query = $bdd->prepare('SELECT `id`, `mois`, `annee` FROM fiche_frais WHERE utilisateur_id = :utilisateur ORDER BY annee DESC, mois DESC');
+$query->bindParam(':utilisateur', $_GET['utilisateur']);
+$query->execute();
+$fiches = $query->fetchAll();
 
 $retour = [];
 for ($i=0; $i < count($fiches); $i++) {
       $retour[$i]['id'] = $fiches[$i]['id'];
       $retour[$i]['mois'] = $fiches[$i]['mois'];
       $retour[$i]['annee'] = $fiches[$i]['annee'];
-      $retour[$i]['etat_id'] = $fiches[$i]['etat_id'];
-      $retour[$i]['utilisateur_id'] = $fiches[$i]['utilisateur_id'];
 }
 
 
